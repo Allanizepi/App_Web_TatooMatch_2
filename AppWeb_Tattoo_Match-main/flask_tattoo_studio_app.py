500c6fd40033cf8c930e5d015f06f4d434eef65b
Allan <allanizepi@gmail.com>
2026-03-24 10:49:57 -0300

    Ajuste de porta para nuvem

--- AppWeb_Tattoo_Match-main/flask_tattoo_studio_app.py
+++ AppWeb_Tattoo_Match-main/flask_tattoo_studio_app.py
@@ -183,5 +183,7 @@ def predict():
         return f"Erro no processamento: {e}", 400
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    # O Render define a porta automaticamente através de uma variável de ambiente
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
